34d8ce586ff515a57e1973b69171b58708894b45
AbhayAzariah <157324011+AbhayAzariah@users.noreply.github.com>
2024-02-10 17:39:57 -0500

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+random_floats = np.random.rand(10)
+
+print("Generated array:", random_floats)
+
+print("Mean:", np.mean(random_floats))
+print("Median:", np.median(random_floats))
+print("Standard Deviation:", np.std(random_floats))
